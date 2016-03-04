7dbd28143ec3107e82068a0f02c7abc634ddcd9d
MatthieuLemoine <ml.94230@gmail.com>
2016-03-04 20:37:28 +0100

    Add parts & clean code

--- index.js
+++ index.js
@@ -4,20 +4,25 @@ let through  = require('through2');
 let duplexer = require('duplexer2');
 let split    = require('split');
 
+// File streams
+let fileStream = fs.createReadStream('./inputs/hacker.txt',{encoding:'utf8'});
+let streams = [
+  fs.createReadStream('./inputs/part1.txt',{encoding:'utf8'}),
+  fs.createReadStream('./inputs/part2.txt',{encoding:'utf8'}),
+  fs.createReadStream('./inputs/part3.txt',{encoding:'utf8'}),
+  fs.createReadStream('./inputs/part4.txt',{encoding:'utf8'})
+];
+
+// Streams
+let str      = require('./streams')(fileStream,streams);
+
 process.title = 'Hacker Typer';
-
-let fileStream = fs.createReadStream('./hacker.txt',{encoding:'utf8'});
-
 process.stdin.resume().setRawMode(true);
 
+// Display first part
+str.streamNextBlock();
+
+// Main stream
 process.stdin
-  .pipe(through(function(buff,_,next){
-    let key = buff.toString();
-    if(key === '\u0003') process.exit();
-    let chunk;
-    if((chunk = fileStream.read(5)) !== null){
-      this.push(chunk);
-    }
-    next();
-  }))
+  .pipe(str.hackStream())
   .pipe(process.stdout);
---
+++ streams.js
@@ -0,0 +1,57 @@
+'use strict';
+let through  = require('through2');
+let split    = require('split');
+
+module.exports = (fileStream,streams) => {
+    return {
+      hackStream      : () => hackStream(fileStream,streams),
+      slowStream      : () => slowStream(streams),
+      streamNextBlock : () => streamNextBlock(streams)
+    };
+};
+
+function hackStream(fileStream,streams){
+  return through(function(buff,_,next){
+    let key = buff.toString();
+    // Ctrl + C
+    if(key === '\u0003') {
+      process.exit();
+    }else{
+        let chunk;
+        if((chunk = fileStream.read(1)) !== null){
+          if(chunk === 'µ'){
+            // Display next block
+            streamNextBlock(streams);
+          }else{
+            this.push(chunk);
+          }
+        }else{
+          process.exit();
+        }
+    }
+    next();
+  });
+}
+
+function slowStream(){
+  return through(function(chunk,_,next){
+    process.stdin.pause();
+    setTimeout(() => {
+      this.push(chunk);
+      next();
+    },10);
+  },function(done){
+    process.stdin.resume();
+    done();
+  });
+}
+
+
+function streamNextBlock(streams){
+  if(streams.length > 0){
+    streams.shift()
+      .pipe(split(/(?!$)/))
+      .pipe(slowStream())
+      .pipe(process.stdout);
+  }
+}
